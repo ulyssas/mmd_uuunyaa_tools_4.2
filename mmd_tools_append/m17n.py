e1e4a25d87bdf16fa1c691ca5a213a111b8b8212
WimPum <n1v0x.5127e@gmail.com>
2026-04-05 16:15:41 +0900

    Release v4.5.9

--- mmd_tools_append/m17n.py
+++ mmd_tools_append/m17n.py
@@ -14,12 +14,12 @@ translations_tuple = (
         ((), ()),
         (
             "ja_JP",
-            "Project-Id-Version: MMD Tools Append 4.5.8 (0)\n",
+            "Project-Id-Version: MMD Tools Append 4.5.9 (0)\n",
             (False, ("Blender's translation file (po format).", "Copyright (C) 2024 The Blender Authors.", "This file is distributed under the same license as the Blender package.", "FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.")),
         ),
         (
             "zh_HANS",
-            "Project-Id-Version: MMD Tools Append 4.5.8 (0)\n",
+            "Project-Id-Version: MMD Tools Append 4.5.9 (0)\n",
             (False, ("Blender's translation file (po format).", "Copyright (C) 2024 The Blender Authors.", "This file is distributed under the same license as the Blender package.", "FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.")),
         ),
     ),
